76b9611dedfa571e590a516de458dbba85e542dc
Placentino Simon <39631@heb.be>
2015-06-02 21:41:10 +0200

    déstruction du code de porc

--- process.c
+++ process.c
@@ -1,22 +1,25 @@
 #define _XOPEN_SOURCE
 
 #include <unistd.h>
+#include <stdlib.h> 
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <wait.h>
+
+#define SEM_UN 0
+#define SEM_DEUX 1
 
 void exit_error(char * message)
 {
 	perror(message);
-	exit(1);
+	exit(EXIT_FAILURE);
 }
 
 void display_msg(char * message)
 {
-	int i;
-	for (i = 0; i < 10; ++i)
+	for (int i = 0; i < 10; ++i)
 	{
 		printf("%s", message);
 		sleep(1);
@@ -28,15 +31,13 @@ int main()
 
 	int pid1, pid2, sem;
 
-	if (sem = semget(IPC_PRIVATE, 2, 0666 | IPC_CREAT) == -1)
+	if ((sem = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666)) == -1)
 		exit_error("Erreur semget");
 
-
-
-	if (semctl(sem, 0, SETVAL, 0) == -1)
+	if (semctl(sem, SEM_UN, SETVAL, 0) == -1)
 		exit_error("erreur semctl 0");
 
-	if (semctl(sem, 1, SETVAL, 0) == -1)
+	if (semctl(sem, SEM_DEUX, SETVAL, 0) == -1)
 		exit_error("erreur semctl 1");
 
 	struct sembuf up;
@@ -54,8 +55,9 @@ int main()
 	{
 		display_msg("Process 1\n");
 
-		up.sem_num = 0;
+		up.sem_num = SEM_UN;
 		semop(sem, &up, 1);
+		exit(EXIT_SUCCESS);
 	}
 	else if (pid1 == -1)
 	{
@@ -67,8 +69,9 @@ int main()
 		if (pid2 == 0)
 		{
 			display_msg("Process 2\n");
-			up.sem_num = 1;
+			up.sem_num = SEM_DEUX;
 			semop(sem, &up, 1);
+			exit(EXIT_SUCCESS);
 		}
 		else if (pid2 == -1)
 		{
@@ -76,18 +79,22 @@ int main()
 		}
 		else
 		{
-			down.sem_num = 0;
+			down.sem_num = SEM_UN;
 			semop(sem, &down, 1);
 
-			down.sem_num = 1;
+			down.sem_num = SEM_DEUX;
 			semop(sem, &down, 1);
 
 			display_msg("Process 3\n");
+
+			while (waitpid(-1, NULL, WNOHANG) > 0);
+
+			if ( semctl(sem, SEM_UN, IPC_RMID) == -1)
+				exit_error("Delete set sem");
 		}
 	}
 
-	return 0;
-
+	exit(EXIT_SUCCESS);
 }
 
 
