023fb4083d4911c3f3572357bf7c3431270459fd
splacentino <39631@heb.be>
2015-08-27 20:18:19 +0200

    Terminé

--- blake_carreau.c
+++ blake_carreau.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <wait.h>
 
@@ -50,23 +51,31 @@ int main()
 
 	while(strncmp(buffer, "exit", sizeof("exit") - 1) != 0)
 	{
-		if (tokens[0] != NULL && strncmp(tokens[0], "mkdir", sizeof("mkdir") -1) == 0)
+		if (tokens[0] != NULL && strncmp(tokens[0], "mkdir", strlen("mkdir")) == 0)
 		{
+				printf("tokens 0 != null \n");
 			if (tokens[1] != NULL) 
 			{
-				if (fork() == 0)
-					execlp(tokens[0], tokens[0], tokens[1], NULL);
-				waitpid(-1, NULL, 0);
+				printf("tokens 1 != null \n");
+				mkdir(tokens[1], 0775);
+			}
+			else
+			{
+				exit_error("Erreur mkdir");
 			}
 
-			if(tokens[2] != NULL && strncmp(tokens[2], "&&", sizeof("&&") - 1))
+			if(tokens[2] != NULL && strncmp(tokens[2], "&&", strlen("&&")) == 0)
 			{
-				if(tokens[3] != NULL & strncmp(tokens[3], "cd", sizeof("cd") - 1))
+				printf("tokens 2 != null \n");
+				if(tokens[3] != NULL & strncmp(tokens[3], "cd", strlen("cd")) == 0)
 				{
+					printf("tokens 3 != null \n");
 					if(tokens[4] != NULL)
 					{
-						if (chdir(tokens[4]) == -1)
+						printf("tokens 4 != null \n");
+						if (chdir(tokens[4]) != 0)
 							exit_error("Erreur de changement de dossier");
+						execlp("blake_carreau", "blake_carreau", NULL);
 					}
 				}
 			}
